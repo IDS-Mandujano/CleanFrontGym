90802aacf57eecf0b5a894627610585532223662
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2025-02-06 22:44:05 -0600

    add delete equipment

--- src/app/presentation/modules/equipment/equipment-list/equipment-list.component.ts
+++ src/app/presentation/modules/equipment/equipment-list/equipment-list.component.ts
@@ -2,18 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { GetAllEquipmentUseCase } from '../../../../core/use-cases/Equipment/get-all-equipment';
 import { Equipment } from '../../../../core/models/equipment';
 import { DeleteEquipmentUseCase } from '../../../../core/use-cases/Equipment/delete-equipment';
+import { UpdateEquipmentUseCase } from '../../../../core/use-cases/Equipment/update-equipment';
 
 @Component({
   selector: 'app-equipment-list',
   templateUrl: './equipment-list.component.html',
   styleUrls: ['./equipment-list.component.css']
 })
+
 export class EquipmentListComponent implements OnInit {
   equipmentList: Equipment[] = [];
+  selectedEquipment: Equipment | null = null;
+  isModalOpen: boolean = false;
 
   constructor(
     private getAllEquipmentUseCase: GetAllEquipmentUseCase,
-    private deleteEquipmentUseCase : DeleteEquipmentUseCase
+    private updateEquipmentUseCase: UpdateEquipmentUseCase,
+    private deleteEquipmentUseCase: DeleteEquipmentUseCase
   ) {}
 
   ngOnInit() {
@@ -29,7 +34,7 @@ export class EquipmentListComponent implements OnInit {
       error: (err) => console.error('Error al obtener equipos:', err)
     });
   }
-  
+
   deleteEquipment(id: number) {
     if (confirm('¿Estás seguro de que quieres eliminar este equipo?')) {
       this.deleteEquipmentUseCase.execute(id).subscribe({
